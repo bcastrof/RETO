7e2a4547971555e4be478fc04dd7e0517635c2f1
Unknown <brunoemanuelcf@gmail.com>
2017-05-09 17:58:09 +0200

    jspin run

--- programacion/Proyecto/src/com/ventanas/Viajes.java
+++ programacion/Proyecto/src/com/ventanas/Viajes.java
@@ -20,9 +20,9 @@ import javax.swing.SpinnerDateModel;
 public class Viajes extends javax.swing.JFrame {
 
     public static BigDecimal id = Login.idt;
-    // private Object JSpinner3;
-    // private SpinnerDateModel sm;
-    // private SpinnerDateModel sm3;
+    private Object JSpinner3;
+    private SpinnerDateModel sm;
+    private SpinnerDateModel sm3;
 
     /**
      * Creates new form Viajes
@@ -30,14 +30,7 @@ public class Viajes extends javax.swing.JFrame {
     public Viajes() {
         initComponents();
         {
-            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date date = new Date();
-            // Date fecha = new Date();
-            jLabel3.setText(dateFormat.format(date));
-            //SpinnerDateModel sm = new SpinnerDateModel(date, null,null, Calendar.HOUR_OF_DAY);
-            //jSpinner1 = new javax.swing.JSpinner(sm);
-            //SpinnerDateModel sm1 = new SpinnerDateModel(fecha, null,null, Calendar.HOUR_OF_DAY);
-            //jSpinner2 = new javax.swing.JSpinner(sm1);
+
         }
 
     }
@@ -54,6 +47,14 @@ public class Viajes extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        java.util.Date date = new java.util.Date();
+        sm =
+        new SpinnerDateModel(date, null, null, Calendar.HOUR_OF_DAY);
+        jsinicio = new javax.swing.JSpinner(sm);
+        java.util.Date date3 = new java.util.Date();
+        sm3 =
+        new SpinnerDateModel(date3, null, null, Calendar.HOUR_OF_DAY);
+        jsfin = new javax.swing.JSpinner(sm3);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -69,20 +70,31 @@ public class Viajes extends javax.swing.JFrame {
             }
         });
 
+        JSpinner.DateEditor de = new JSpinner.DateEditor(jsinicio, "HH:mm");
+        jsinicio.setEditor(de);
+
+        JSpinner.DateEditor de3 = new JSpinner.DateEditor(jsfin, "HH:mm");
+        jsfin.setEditor(de3);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(128, 128, 128)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jsinicio, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
+                            .addComponent(jsfin))
+                        .addGap(52, 52, 52)
+                        .addComponent(jButton1)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(47, 47, 47))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -91,7 +103,11 @@ public class Viajes extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(130, 130, 130)
+                .addGap(46, 46, 46)
+                .addComponent(jsinicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jsfin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
                 .addComponent(jButton1)
                 .addContainerGap(189, Short.MAX_VALUE))
         );
@@ -100,8 +116,12 @@ public class Viajes extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       
-      
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+
+        String horaini = dateFormat.format(sm.getDate());
+        String horafin = dateFormat.format(sm3.getDate());
+          System.out.println(horaini+" "+horafin);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -143,5 +163,7 @@ public class Viajes extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JSpinner jsfin;
+    private javax.swing.JSpinner jsinicio;
     // End of variables declaration//GEN-END:variables
 }
